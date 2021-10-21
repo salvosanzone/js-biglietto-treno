2e400d89f906fe013cbf044cd2814b0eb8c8e33e
salvosanzone <salvosansone.ss@gmail.com>
2021-10-21 17:34:40 +0200

    inserimento output

--- js/script.js
+++ js/script.js
@@ -1,11 +1,39 @@
+
+
 const percorrenzaKm = parseInt(prompt('Quanti km devi percorrere?'));
-const età = parseInt(prompt('Quanti anni hai?'));
+const eta = parseInt(prompt('Quanti anni hai?'));
 
 console.log('km percossi ',percorrenzaKm);
-console.log('età passeggero ',età);
+console.log('età passeggero ',eta);
 
+const prizeOneKm = 0.21;
 
-const prizeOneKm = 0.21 ;
-
-const prodottoKmEuro = percorrenzaKm * prizeOneKm;
+let prodottoKmEuro = percorrenzaKm * prizeOneKm;
 console.log('prezzo per km percosso',prodottoKmEuro);
+
+const minorenne = 17;
+if (eta <= minorenne) {
+  console.log("Ok, utente minorenne");
+  prodottoKmEuro = prodottoKmEuro - prodottoKmEuro * 0.20
+}
+
+const over = 65;
+if (eta > over) {
+  console.log("Ok, utente over 65");
+  prodottoKmEuro = prodottoKmEuro - prodottoKmEuro * 0.40
+}
+
+console.log('prezzo finale',prodottoKmEuro);
+
+ 
+const textToPrint =
+`
+Quanti km devi percorrere? ${percorrenzaKm};<br>
+Quanti anni hai? ${eta};<br>
+Prezzo finale: ${prodottoKmEuro}
+`;
+console.log(textToPrint);
+document.getElementById('output').innerHTML = textToPrint;
+
+
+  // return finalPrice.toFixed(2);
